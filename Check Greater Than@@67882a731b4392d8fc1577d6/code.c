aa7d71cc85d2073db9b2989f53988f4cb72d8227
RITISHM <70802345+RITISHM@users.noreply.github.com>
2025-02-08 21:24:35 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+#include<stdbool.h>
 
 int main() {
-    bool result=1;
-    printf("%s", welcome());
+    bool result=false;
+    int a,b;
+    scanf("%d %d",&a,&b);
+    result= a>b;
+    printf("%b", result);
     return 0;
 }
